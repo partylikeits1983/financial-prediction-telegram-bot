1960068d90cc19ad01b18aceb5671520d66087a3
partylikeits1983 <77119221+partylikeits1983@users.noreply.github.com>
2021-01-28 11:10:47 -0500

    Add files via upload

---
+++ tensorflow/pricepredictionXMR.py
@@ -0,0 +1,238 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout, Bidirectional
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from yahoo_fin import stock_info as si
+from collections import deque
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+import os
+import random
+import datetime
+from datetime import date
+
+today = date.today()
+d = datetime.timedelta(days = 180)
+start = today - d
+
+
+# set seed, so we can get the same results after rerunning several times
+np.random.seed(314)
+tf.random.set_seed(314)
+random.seed(314)
+
+def load_data(ticker, n_steps=70, scale=True, shuffle=True, lookup_step=1,
+                test_size=0.35, feature_columns=['adjclose', 'volume', 'open', 'high', 'low']):
+    """
+    Loads data from Yahoo Finance source, as well as scaling, shuffling, normalizing and splitting.
+    Params:
+        ticker (str/pd.DataFrame): the ticker you want to load, examples include AAPL, TESL, etc.
+        n_steps (int): the historical sequence length (i.e window size) used to predict, default is 50
+        scale (bool): whether to scale prices from 0 to 1, default is True
+        shuffle (bool): whether to shuffle the data, default is True
+        lookup_step (int): the future lookup step to predict, default is 1 (e.g next day)
+        test_size (float): ratio for test data, default is 0.2 (20% testing data)
+        feature_columns (list): the list of features to use to feed into the model, default is everything grabbed from yahoo_fin
+    """
+    # see if ticker is already a loaded stock from yahoo finance
+    if isinstance(ticker, str):
+        # load it from yahoo_fin library
+        df = si.get_data(ticker)
+    elif isinstance(ticker, pd.DataFrame):
+        # already loaded, use it directly
+        df = ticker
+    else:
+        raise TypeError("ticker can be either a str or a `pd.DataFrame` instances")
+    # this will contain all the elements we want to return from this function
+    result = {}
+    # we will also return the original dataframe itself
+    result['df'] = df.copy()
+    # make sure that the passed feature_columns exist in the dataframe
+    for col in feature_columns:
+        assert col in df.columns, f"'{col}' does not exist in the dataframe."
+    if scale:
+        column_scaler = {}
+        # scale the data (prices) from 0 to 1
+        for column in feature_columns:
+            scaler = preprocessing.MinMaxScaler()
+            df[column] = scaler.fit_transform(np.expand_dims(df[column].values, axis=1))
+            column_scaler[column] = scaler
+        # add the MinMaxScaler instances to the result returned
+        result["column_scaler"] = column_scaler
+    # add the target column (label) by shifting by `lookup_step`
+    df['future'] = df['adjclose'].shift(-lookup_step)
+    # last `lookup_step` columns contains NaN in future column
+    # get them before droping NaNs
+    last_sequence = np.array(df[feature_columns].tail(lookup_step))
+    # drop NaNs
+    df.dropna(inplace=True)
+    sequence_data = []
+    sequences = deque(maxlen=n_steps)
+    for entry, target in zip(df[feature_columns].values, df['future'].values):
+        sequences.append(entry)
+        if len(sequences) == n_steps:
+            sequence_data.append([np.array(sequences), target])
+    # get the last sequence by appending the last `n_step` sequence with `lookup_step` sequence
+    # for instance, if n_steps=50 and lookup_step=10, last_sequence should be of 60 (that is 50+10) length
+    # this last_sequence will be used to predict future stock prices not available in the dataset
+    last_sequence = list(sequences) + list(last_sequence)
+    last_sequence = np.array(last_sequence)
+    # add to result
+    result['last_sequence'] = last_sequence
+    # construct the X's and y's
+    X, y = [], []
+    for seq, target in sequence_data:
+        X.append(seq)
+        y.append(target)
+    # convert to numpy arrays
+    X = np.array(X)
+    y = np.array(y)
+    # reshape X to fit the neural network
+    X = X.reshape((X.shape[0], X.shape[2], X.shape[1]))
+    # split the dataset
+    result["X_train"], result["X_test"], result["y_train"], result["y_test"] = train_test_split(X, y,
+                                                                               test_size=test_size, shuffle=shuffle)
+    # return the result
+    return result
+def create_model(sequence_length, units=256, cell=LSTM, n_layers=2, dropout=0.3,
+                loss="mean_absolute_error", optimizer="rmsprop", bidirectional=False):
+    model = Sequential()
+    for i in range(n_layers):
+        if i == 0:
+            # first layer
+            if bidirectional:
+                model.add(Bidirectional(cell(units, return_sequences=True), input_shape=(None, sequence_length)))
+            else:
+                model.add(cell(units, return_sequences=True, input_shape=(None, sequence_length)))
+        elif i == n_layers - 1:
+            # last layer
+            if bidirectional:
+                model.add(Bidirectional(cell(units, return_sequences=False)))
+            else:
+                model.add(cell(units, return_sequences=False))
+        else:
+            # hidden layers
+            if bidirectional:
+                model.add(Bidirectional(cell(units, return_sequences=True)))
+            else:
+                model.add(cell(units, return_sequences=True))
+        # add dropout after each layer
+        model.add(Dropout(dropout))
+    model.add(Dense(1, activation="linear"))
+    model.compile(loss=loss, metrics=["mean_absolute_error"], optimizer=optimizer)
+    return model
+
+# Window size or the sequence length
+N_STEPS = 1000
+# Lookup step, 1 is the next day
+LOOKUP_STEP = 1
+# test ratio size, 0.2 is 20%
+TEST_SIZE = 0.2
+# features to use
+FEATURE_COLUMNS = ["adjclose", "volume", "open", "high", "low"]
+# date now
+date_now = time.strftime("%Y-%m-%d")
+### model parameters
+N_LAYERS = 3
+# LSTM cell
+CELL = LSTM
+# 256 LSTM neurons
+UNITS = 512
+# 40% dropout
+DROPOUT = 0.4
+# whether to use bidirectional RNNs
+BIDIRECTIONAL = False
+### training parameters
+# mean absolute error loss
+# LOSS = "mae"
+# huber loss
+LOSS = "huber_loss"
+OPTIMIZER = "adam"
+BATCH_SIZE = 64
+EPOCHS = 60
+# Ticker
+ticker = "XMR-USD"
+ticker_data_filename = os.path.join("/home/ubuntu/Desktop/TelegramBot/tensorflow/tensorflowdata/dataXMR", f"{ticker}_{date_now}.csv")
+# model name to save, making it as unique as possible based on parameters
+model_name = f"{date_now}_{ticker}-{LOSS}-{OPTIMIZER}-{CELL.__name__}-seq-{N_STEPS}-step-{LOOKUP_STEP}-layers-{N_LAYERS}-units-{UNITS}"
+if BIDIRECTIONAL:
+    model_name += "-b"
+
+    # create these folders if they does not exist
+if not os.path.isdir("/home/ubuntu/Desktop/TelegramBot/tensorflow/tensorflowdata/resultsXMR"):
+    os.mkdir("/home/ubuntu/Desktop/TelegramBot/tensorflow/tensorflowdata/resultsXMR")
+if not os.path.isdir("/home/ubuntu/Desktop/TelegramBot/tensorflow/tensorflowdata/logsXMR"):
+    os.mkdir("/home/ubuntu/Desktop/TelegramBot/tensorflow/tensorflowdata/logsXMR")
+if not os.path.isdir("/home/ubuntu/Desktop/TelegramBot/tensorflow/tensorflowdata/dataXMR"):
+    os.mkdir("/home/ubuntu/Desktop/TelegramBot/tensorflow/tensorflowdata/dataXMR")
+
+# load the data
+data = load_data(ticker, N_STEPS, lookup_step=LOOKUP_STEP, test_size=TEST_SIZE, feature_columns=FEATURE_COLUMNS)
+# save the dataframe
+data["df"].to_csv(ticker_data_filename)
+# construct the model
+model = create_model(N_STEPS, loss=LOSS, units=UNITS, cell=CELL, n_layers=N_LAYERS,
+                    dropout=DROPOUT, optimizer=OPTIMIZER, bidirectional=BIDIRECTIONAL)
+# some tensorflow callbacks
+checkpointer = ModelCheckpoint(os.path.join("/home/ubuntu/Desktop/TelegramBot/tensorflow/tensorflowdata/resultsXMR", model_name + ".h5"), save_weights_only=True, save_best_only=True, verbose=1)
+tensorboard = TensorBoard(log_dir=os.path.join("/home/ubuntu/Desktop/TelegramBot/tensorflow/tensorflowdata/logsXMR", model_name))
+history = model.fit(data["X_train"], data["y_train"],
+                    batch_size=BATCH_SIZE,
+                    epochs=EPOCHS,
+                    validation_data=(data["X_test"], data["y_test"]),
+                    callbacks=[checkpointer, tensorboard],
+                    verbose=1)
+model.save(os.path.join("/home/ubuntu/Desktop/TelegramBot/tensorflow/tensorflowdata/resultsXMR", model_name) + ".h5")
+
+data = load_data(ticker, N_STEPS, lookup_step=LOOKUP_STEP, test_size=TEST_SIZE,
+                feature_columns=FEATURE_COLUMNS, shuffle=False)
+
+# evaluate the model
+mse, mae = model.evaluate(data["X_test"], data["y_test"], verbose=0)
+# calculate the mean absolute error (inverse scaling)
+mean_absolute_error = data["column_scaler"]["adjclose"].inverse_transform([[mae]])[0][0]
+print("Mean Absolute Error:", mean_absolute_error)
+
+######################PATH
+
+
+a = str(round(mean_absolute_error, 2))
+
+b = "{}".format(a)
+
+file = open('/home/ubuntu/Desktop/TelegramBot/predictions/meanabsoluteerrorXMR.txt', 'w')
+file.write(b)
+file.close()
+
+
+def predict(model, data):
+    # retrieve the last sequence from data
+    last_sequence = data["last_sequence"][-N_STEPS:]
+    # retrieve the column scalers
+    column_scaler = data["column_scaler"]
+    # reshape the last sequence
+    last_sequence = last_sequence.reshape((last_sequence.shape[1], last_sequence.shape[0]))
+    # expand dimension
+    last_sequence = np.expand_dims(last_sequence, axis=0)
+    # get the prediction (scaled from 0 to 1)
+    prediction = model.predict(last_sequence)
+    # get the price (by inverting the scaling)
+    predicted_price = column_scaler["adjclose"].inverse_transform(prediction)[0][0]
+    return predicted_price
+
+future_price = predict(model, data)
+print(f"Future price after {LOOKUP_STEP} days is {future_price:.2f}$")
+
+c = str(round(future_price, 2))
+
+d = "{}".format(c)
+
+file = open('/home/ubuntu/Desktop/TelegramBot/predictions/futurepriceXMR.txt', 'w')
+file.write(d)
+file.close()
