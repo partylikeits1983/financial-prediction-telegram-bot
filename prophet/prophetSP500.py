bfbd258fde28ac993ed9ba4029317c242fbf6ecb
Alexander Lee <77119221+partylikeits1983@users.noreply.github.com>
2021-07-21 23:00:11 +0300

    Update prophetSP500.py

--- prophet/prophetSP500.py
+++ prophet/prophetSP500.py
@@ -1,49 +1,36 @@
+from yahoo_fin import stock_info as si
 import pandas as pd
-from pandas_datareader import data, wb  # Package and modules for importing data; this code may change depending on pandas version
-import matplotlib.pyplot as plt   
-    
-import datetime
-from datetime import date 
-import numpy as np
 from fbprophet import Prophet
-    
+from fbprophet.plot import add_changepoints_to_plot
+
 end = date.today()
-d = datetime.timedelta(days=365)
+d = datetime.timedelta(days=730)
 start = end - d
-    
-graph = data.DataReader("^GSPC", "yahoo", start, end)
- 
-type(graph)
 
-graph.plot(grid = True)
-stock_return = graph.apply(lambda x: x / x[0])
-stock_return.head()
-stock_return.plot(grid = True).axhline(y = 1, color = "black", lw = 2)
-stock_change = graph.apply(lambda x: np.log(x) - np.log(x.shift(1))) # shift moves dates back by 1.
-stock_change.head()
-stock_change.plot(grid = True).axhline(y = 0, color = "black", lw = 2)
-graph["20d"] = np.round(graph["Close"].rolling(window = 20, center = False).mean(), 2)
-
+ticker = "SPY"
+s = si.get_data(ticker, start, end)
+s['Date'] = s.index
+s.rename({'close': 'Close'}, axis=1, inplace=True)
+s.head()
 
 df = pd.DataFrame()
-df['ds'] = stock_return.index
-df['y']=graph['Close'].apply(lambda x: np.log(x)).values
-df.tail()
+df['ds'] = (s['Date'])
+df['y'] = s['Close']
+df.head()
 
-m0 = Prophet(yearly_seasonality=True)
-m0.fit(df)
-#how many days in the future to show predictions for 
-n_add = 100
-print("adding {n} days.".format(n=n_add))
-future = m0.make_future_dataframe(periods=n_add) 
-future.tail()
+m = Prophet()
+m.fit(df)
 
-forecast = m0.predict(future)
-forecast[['ds','yhat','yhat_lower','yhat_upper']].tail()
-forcast = m0.plot(forecast, ylabel='$\ln($stock_return$)$');
-forcast.savefig('/home/ubuntu/Desktop/TelegramBot/charts/SP500forcast.jpeg', dpi=400, bbox_inches='tight')
+future = m.make_future_dataframe(periods=12 * 8, freq='D')
+forecast = m.predict(future)
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper', 'trend', 'trend_lower', 'trend_upper']].tail()
 
+fig1 = m.plot(forecast)
+fig1.savefig('/home/ubuntu/Desktop/TelegramBot/charts/SP500forcast.jpeg', dpi=400, bbox_inches='tight')
 
-trend = m0.plot_components(forecast);
-trend.savefig('/home/ubuntu/Desktop/TelegramBot/charts/SP500trend.jpeg', dpi=400, bbox_inches='tight')
+fig2 = m.plot_components(forecast)
+fig2.savefig('/home/ubuntu/Desktop/TelegramBot/charts/SP500trend.jpeg', dpi=400, bbox_inches='tight')
 
+fig = m.plot(forecast)
+a = add_changepoints_to_plot(fig.gca(), m, forecast)
+fig.savefig('/home/ubuntu/Desktop/TelegramBot/charts/SP500forcastwithlines.jpeg', dpi=400, bbox_inches='tight')
